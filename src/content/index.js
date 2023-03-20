3ed1b621396ce0561734c68e0ada2ecd53443891
Santosh177 <santosh08011999@gmail.com>
2023-03-20 16:46:41 +0530

    profile done

---
+++ src/content/index.js
@@ -0,0 +1,69 @@
+
+export const skills=[
+  'React',
+  'Angular',
+  'Vue',
+  'Node',
+  'Express',
+  'MongoDB',
+  'MySQL',
+  'PostgreSQL',
+  'Python',
+  'Java',
+  'C++',
+  'C#',
+  'C',
+  'Ruby',
+]
+export const experience=[
+  '0-1',
+  '1-2',
+  '2-3',
+  '3-4',
+  '4-5',
+  '5-6',
+  '6+'
+]
+export const primaryRole=[
+  'Frontend',
+  'Backend',
+  'Fullstack',
+  'DevOps',
+  'QA',
+  'UI/UX',
+  'Data Science',
+  'Mobile',
+  'Other',
+]
+export const industryType=[
+  'Agriculture',
+  'Automotive',
+  'Banking',
+  'Biotechnology',
+  'Chemicals',
+  'Computer Hardware',
+  'Computer Software',
+  'Construction',
+  'Consulting',
+  'Education',
+  'Electronics',
+  'Energy',
+  'Engineering',
+  'Entertainment',
+  'Environmental',
+
+  'Finance',
+  'Food & Beverages',
+  'Government',
+  'Healthcare',
+  'Hospitality',
+]
+
+export const companySize=[
+  '1-10',
+  '11-50',
+  '51-100',
+  '100-500',
+  '500-1000',
+  '100+',
+]
